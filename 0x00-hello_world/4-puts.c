7ee992903f9dc4a9586b79de33b269e08432b136
Alecora <alexis_ca_@outlook.com>
2021-02-04 03:24:10 +0000

    Programa en C con función puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-/*using puts to print a text*/
+
+/**
+ *  puts - function to print in terminal
+ * 
+ *  
+ * Return: Value 0 when correct
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
