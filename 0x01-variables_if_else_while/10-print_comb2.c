c43e061f2d450992bf3de51a4606a03229222785
Alecora <alexis_ca_@outlook.com>
2021-02-06 04:32:38 +0000

    Imprimir números del 0 al 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints the numbers from 00 to 99
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 9 && num2 == 9)
+				break;
+			
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
