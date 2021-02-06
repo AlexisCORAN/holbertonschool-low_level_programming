32ef62ba62cd09f47ba0014965e3c6303afcc065
Alecora <alexis_ca_@outlook.com>
2021-02-06 01:38:55 +0000

    Programa para imprimir abecedario en minúsculas

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+* main - print lowercase alphabet
+*
+* Return: 0 is success
+*/
+int main(void)
+{
+	int letter;
+
+	letter = 'a'
+	while (letter <= 'z')
+	{
+		putchar(letter++);
+	}
+	putchar ('\n')
+	return (0);
+}
