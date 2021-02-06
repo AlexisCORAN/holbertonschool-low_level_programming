df99b51b3d5885253173afae4cf50ddd15205f55
Alecora <alexis_ca_@outlook.com>
2021-02-06 04:51:49 +0000

    Combinaciones de dos dígitos

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,22 +8,21 @@
  */
 int main(void)
 {
-        int num1;
-        int num2;
+	int num1;
+	int num2;
 
-        for (num1 = 0; num1 < 9; num1++)
-        {
-                for (num2= num1 + 1; num2 < 10; num2++)
-                {
-                        putchar((num1 % 10) + '0');
-                        putchar((num2 % 10) + '0');
-                        if (num1 == 8 && num2 == 9)
-                                break;
-                        putchar(',');
-                        putchar(' ');
-                }
-        }
-
-        putchar('\n');
-        return (0);
+	for (num1 = 0; num1 < 9; num1++;
+	{
+		for (num2 = num1 + 1; num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 8 && num2 == 9)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
