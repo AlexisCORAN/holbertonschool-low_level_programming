5a8ddff1b40dfd24d4ed325dceec4d39cf8e77b0
Alecora <alexis_ca_@outlook.com>
2021-02-09 14:16:41 +0000

    Imprimir 10 veces abecedario

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* print_alphabet_x10 -  prints 10 times the alphabet, in lowercase
+*
+* Return: no explicit declaration necessary
+**/
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int count;
+	for (count = 0; count < 10; count++)
+	{
+		for (letter = 'a';letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
