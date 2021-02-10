55305c34d5aa34c90630fea4dc1434a50aca1624
Alecora <alexis_ca_@outlook.com>
2021-02-10 04:22:23 +0000

    sumar enteros

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* add - two integers and returns the result
+*
+* @n1: the character to be checked
+* @n2: the character to be checked
+*
+* Return: 0
+*/
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
