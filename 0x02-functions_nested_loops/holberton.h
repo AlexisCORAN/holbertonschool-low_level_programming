7bc484d934c03de29f5fd214628d51b2699e9b7c
Alecora <alexis_ca_@outlook.com>
2021-02-10 04:07:15 +0000

    Actualizacióin archivo cabezera

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -6,5 +6,11 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+int add(int n1, int n2);
+void print_to_98(int n);
 
 #endif /* _HOLBERTON_H_ */
