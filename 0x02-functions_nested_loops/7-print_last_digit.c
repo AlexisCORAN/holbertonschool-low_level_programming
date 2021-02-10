b3274e92ee688e1e3f126c82b5360f51d1b34992
Alecora <alexis_ca_@outlook.com>
2021-02-10 04:14:23 +0000

    último digito

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,16 @@
 # include "holberton.h"
-
+/**
+* print_last_digit - check the last digit
+*
+* @n: the character to be checked
+*
+* Return: 0
+*/
 int print_last_digit(int n)
 {
 	int ld = n % 10;
 
-	if ( ld < 0)
+	if (ld < 0)
 	{
 		ld *= -1;
 	}
