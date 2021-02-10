db00578d41da6ba82b3469160b7a957347b6babb
Alecora <alexis_ca_@outlook.com>
2021-02-10 03:53:34 +0000

    Valor absoluto de enteros

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _abs - check the absolute value of an integer
+*
+* @n: the character to be checked
+*
+* Return: n show absolute value
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
