3f24320929acbc4aa67cfa216a28959e2f13f803
AlexisCORAN <alexis_ca_@outlook.com>
2021-02-12 04:54:43 +0000

    ejercicios

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,20 +1,21 @@
 #include "holberton.h"
 
 /**
-* _isdigit - checks for a digit 
+* _isdigit - checks for a digit
 *
 * @c: the character to be checked
+*
 * Return: 0 or 1 as required
 */
 int _isdigit(int c)
 {
-        if (c >= 48 && c <= 57)
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
