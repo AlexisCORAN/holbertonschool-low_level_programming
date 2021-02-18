544562e8c97b8fcfc1c9cf5dc15a7214b82452f4
AlexisCORAN <alexis_ca_@outlook.com>
2021-02-18 04:50:12 +0000

    print cada 2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,25 +1,22 @@
 #include "holberton.h"
 /**
-* puts_half -
-* 
-*@str: 
+* puts_half - prints half of a string.
+*
+*@str: numbers.
 */
 void puts_half(char *str)
 {
 	int index = 0;
-       	int len = 0,
+	int len = 0,
 	int n;
-	
+
 	while (str[index++])
 		len++;
-
 	if ((len % 2) == 0)
 		n = len / 2;
 	else
 		n = (len + 1) / 2;
-
 	for (index = n; index < len; index++)
 		_putchar(str[index]);
-
 		_putchar('\n');
 }
