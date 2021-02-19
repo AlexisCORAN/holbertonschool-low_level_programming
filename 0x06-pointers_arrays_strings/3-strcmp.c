7d977b64d5177b5c39d8dda9023adae4ee30651b
AlexisCORAN <alexis_ca_@outlook.com>
2021-02-19 03:43:04 +0000

    Comparar 2 cadenas

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,19 +12,21 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	for (i = 0; s1[i] == s2[i] && s1[i] == '\0'; i++)
+	while (s1[i] == s2[i] && s1[i] == '\0')
 	{
-		if (s1[i] < s2[i])
-		{
-			return (-15);
-		}
-		else if (s1[i] > s2[i])
-		{
-			return (15);
-		}
-		else
-		{
-			return (0);
-		}
+		i++;
+	}
+
+	if (s1[i] < s2[i])
+	{
+		return (-15);
+	}
+	else if (s1[i] > s2[i])
+	{
+		return (15);
+	}
+	else
+	{
+		return (0);
 	}
 }
