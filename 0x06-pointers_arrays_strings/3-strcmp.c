d99a44017f07ada5f62d487b4e729c39f841786a
AlexisCORAN <alexis_ca_@outlook.com>
2021-02-19 03:23:10 +0000

    Comparar 2 cadenas

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+* _strcmp - function that compares two strings.
+*
+*@s1: string.
+*
+*@s2: string.
+*
+*Return: numbers.
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	for (i = 0; s1[i] == s2[i] && s1[i] == '\0'; i++)
+	{
+		if (s1[i] < s2[i])
+		{
+			return (-15);
+		}
+		else if (s1[i] > s2[i])
+		{
+			return (15);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
