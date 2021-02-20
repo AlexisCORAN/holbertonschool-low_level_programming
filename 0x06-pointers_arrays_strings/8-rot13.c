5e0f6496a68bd66eff045d7f08e874af365b26c3
AlexisCORAN <alexis_ca_@outlook.com>
2021-02-20 04:48:36 +0000

    Codificacion rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+* rot13 - encodes a string using rot13.
+*
+* @str: pointer to string
+*
+* Return: enconde to the string
+ */
+
+char *rot13(char *str)
+{
+
+	int i, j;
+
+	char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+			'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+			'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
+			'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+			'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	char pair[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+			'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+			'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+			'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E',
+			'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			for (j = 0; str[i] != alphabet[j]; j++)
+			{}
+			str[i] = pair[j];
+		}
+	}
+	return (str);
+}
