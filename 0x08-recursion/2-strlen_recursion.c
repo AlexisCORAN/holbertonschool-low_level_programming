7794ccde796c46d2e8e7a3878a28ce45f5bf4ec7
AlexisCORAN <alexis_ca_@outlook.com>
2021-02-25 04:05:03 +0000

    Longitud de string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _strlen_recursion -  function that returns the length of a string.
+*
+*@s: pointer character.
+*
+*Return: 0 or recursion of *s.
+*
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+		return  (_strlen_recursion(s + 1) + 1);
+}
