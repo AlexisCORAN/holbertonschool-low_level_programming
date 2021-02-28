641705a759a2a5f97e80932e376a12e06833b942
AlexisCORAN <alexis_ca_@outlook.com>
2021-02-28 02:37:11 +0000

    Copiar string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,8 +12,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int index;
 
-	for (index = 0; src[index] = '\0'; index++)
+	for (index = 0; src[index]; index++)
 		dest[index] = src[index];
+
 	dest[index] = '\0';
 	return (dest);
 }
