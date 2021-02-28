3ca33b97aeeb3a9f449bbd8685c0c76919b9d8f0
AlexisCORAN <alexis_ca_@outlook.com>
2021-02-28 19:52:43 +0000

    task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char **argv)
+/**
+* main - program that prints its name, followed by a new line.
+*
+*@argc: refers to the number of command line arguments.
+*
+*@argv: contains the actual arguments.
+*
+*Return: 0 is success.
+*/
+
+int main(int argc, char *argv[])
 {
-        while(argc--)
-                printf("%s\n", *argv++);
-        exit(EXIT_SUCCESS);
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
 }
