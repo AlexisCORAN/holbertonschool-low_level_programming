84b516a2c75ea821dadaf58bf9cb39bddda59263
AlexisCORAN <alexis_ca_@outlook.com>
2021-03-08 22:50:10 +0000

    Task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,12 @@
 #define _DOG_H_
 
 /**
-* struct dog - description of an animal.
-*
+* struct dog - description of a dog.
 *@name: name of a dog.
-*
 *@age: age of a dog.
-*
 *@owner: owner of a dog.
+*
+* Description: description of a dog.
 */
 
 struct dog
@@ -18,10 +17,5 @@ struct dog
 	float age;
 	char *owner;
 };
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif /* _DOG_H_ */
