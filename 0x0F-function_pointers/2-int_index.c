3788f0d686ad644d4eced0b67192c65a83786449
AlexisCORAN <alexis_ca_@outlook.com>
2021-03-11 05:12:15 +0000

    Task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,13 +7,19 @@
 *@size: is the number of elements in the array.
 *
 *@cmp:  is a pointer to the function to be used to compare values.
+*
+*Return: the index of the first element for which the cmp function.
+*
 */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || array == NULL || cmp == NULL)
+	if (size <= 0)
+		return (-1);
+
+	if (array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
