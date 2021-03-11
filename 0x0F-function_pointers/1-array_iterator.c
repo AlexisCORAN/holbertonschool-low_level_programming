d412e8eb57c639e3eaddbbfd7a5be547150d7beb
AlexisCORAN <alexis_ca_@outlook.com>
2021-03-11 04:48:42 +0000

    Task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,16 +1,25 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
+/**
+* array_iterator - function that executes a function given as
+* a parameter on each element of an array.
+*
+*@array: pointer of array.
+*
+*@size: is size of array.
+*
+*@action: is a pointer to the function you need to use.
+*/
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array == NULL || action == NULL || size < 0 )
+	if (array == NULL || action == NULL || size < 0)
 	{
 		return;
 	}
-
-	for(i = 0;i < size; i++)
+	for (i = 0; i < size ; i++)
 	{
 		action(array[i]);
 	}
