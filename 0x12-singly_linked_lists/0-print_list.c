c531f5becb7a1b648baf4081f15cfbe13e9d09f2
AlexisCORAN <alexis_ca_@outlook.com>
2021-03-19 04:10:40 +0000

    task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,21 +13,19 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if( h != NULL)
+		if (h->str == NULL)
 		{
-			if (h->str == NULL)
-			{
-				printf("[%d] %s\n", 0, "(nill)");
-			}
-			else
-			{
-				printf("[%d] %s\n", h->len, h->str);
-			}
+			printf("[%d] %s\n", 0, "(nill)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 
-			h = h->next;
-			count++;
+		h = h->next;
+		count++;
 		}
 	}	
 	return (count);
