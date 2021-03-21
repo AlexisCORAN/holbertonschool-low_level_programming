0948284a8cc6815a6e1d5cbc87f4229637c8432f
AlexisCORAN <alexis_ca_@outlook.com>
2021-03-21 03:49:33 +0000

    Advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* print_before_main - function that prints before the main function
+* is executed.
+*
+*/
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
