bc4be499f7e3ebe32276a7ba04576d765988e66e
AlexisCORAN <alexis_ca_@outlook.com>
2021-03-24 03:06:48 +0000

    Task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* free_listint2 - function that frees a listint_t list.
+*
+*@head: linked list.
+*
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *actual_node = *head;
+	listint_t *next_node;
+
+	while (actual_node != NULL)
+	{
+		next_node = actual_node->next;
+		actual_node->next = NULL;
+		free(actual_node);
+		actual_node = next_node;
+	}
+	*head = NULL;
+}
