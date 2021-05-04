118a03161b8f7a5cdb69bd05f6faf8f34951eb1c
AlexisCORAN <alexis_ca_@outlook.com>
2021-05-04 00:19:20 +0000

    Update header file

--- 0x18-dynamic_libraries/holberton.h
+++ 0x18-dynamic_libraries/holberton.h
@@ -1,6 +1,8 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+#include <stdlib.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif _HOLBERTON_H_
+#endif /* _HOLBERTON_H_ */
